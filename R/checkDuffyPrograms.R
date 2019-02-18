ee21ac2b8b97bf6ecbfc7b6ff8343f45c8832cfe
Robert Morrison <robert.morrison@nih.gov>
2019-02-18 11:11:15 -0800

    more improvements to checking third party software & versions

--- R/checkDuffyPrograms.R
+++ R/checkDuffyPrograms.R
@@ -13,6 +13,7 @@
 		Nfail <- Nfail + 1
 	} else {
 		cat( "\nBowtie2:   version =", ans, "   path =", as.character(Sys.which("bowtie2")))
+		Nfound <- Nfound + 1
 	}
 
 	# samtools
@@ -24,8 +25,10 @@
 		ans <- system( "samtools --version", intern=T)
 		if ( length(ans) == 3) {
 			cat( "\nsamtools:  version =", sub("samtools ","",ans[1]), "       path =", exe)
+			Nfound <- Nfound + 1
 		} else {
 			cat( "\nsamtools:  unexpected reply;\n", ans)
+			Nfail <- Nfail + 1
 		}
 	}
 
@@ -38,8 +41,44 @@
 		ans <- system( "bcftools --version", intern=T)
 		if ( length(ans) == 6) {
 			cat( "\nbcftools:  version =", sub("bcftools ","",ans[1]), "       path =", exe)
+			Nfound <- Nfound + 1
 		} else {
 			cat( "\nbcftools:  unexpected reply;\n", ans)
+			Nfail <- Nfail + 1
+		}
+	}
+
+	# python
+	exe <- Sys.which( "python2")
+	if ( exe == "") {
+		cat( "\n'python2' is not in the current search path..")
+		Nfail <- Nfail + 1
+	} else {
+		ans <- system( "python2 --version  2>&1", intern=T)
+		#cat( "\nDebug: ", exe, "|", length(ans), "|", ans, "\n")
+		if ( length(ans) == 1) {
+			cat( "\npython2:   version =", sub("Python ","",ans[1]), "       path =", exe)
+			Nfound <- Nfound + 1
+		} else {
+			cat( "\npython2:  unexpected reply;\n", ans)
+			Nfail <- Nfail + 1
+		}
+	}
+
+	# perl
+	exe <- Sys.which( "perl")
+	if ( exe == "") {
+		cat( "\n'perl' is not in the current search path..")
+		Nfail <- Nfail + 1
+	} else {
+		ans <- system( "perl --version", intern=T)
+		if ( length(ans) > 1) {
+			ans <- sub( " built.+", "", sub("This is perl ", "", ans[2]))
+			cat( "\nperl:      version =", ans, "       path =", exe)
+			Nfound <- Nfound + 1
+		} else {
+			cat( "\nperl:  unexpected reply;\n", ans)
+			Nfail <- Nfail + 1
 		}
 	}
 
@@ -52,8 +91,10 @@
 		ans <- system( "mafft --version  2>&1", intern=T)
 		if ( length(ans) == 1) {
 			cat( "\nmafft:     version =", ans, "    path =", exe)
+			Nfound <- Nfound + 1
 		} else {
 			cat( "\nmafft:     unexpected reply;\n", ans)
+			Nfail <- Nfail + 1
 		}
 	}
 
@@ -66,8 +107,10 @@
 		ans <- system( "velveth", intern=T) # no explict 'version arg, grab 2nd line
 		if ( length(ans) > 1) {
 			cat( "\nvelveth:   version =", sub("Version ","",ans[2]), "    path =", exe)
+			Nfound <- Nfound + 1
 		} else {
 			cat( "\nvelveth:   unexpected reply;\n", ans)
+			Nfail <- Nfail + 1
 		}
 	}
 	exe <- Sys.which( "velvetg")
@@ -78,8 +121,10 @@
 		ans <- suppressWarnings( system( "velvetg", intern=T)) # no explict 'version arg, grab 2nd line
 		if ( length(ans) > 1) {
 			cat( "\nvelvetg:   version =", sub("Version ","",ans[2]), "    path =", exe)
+			Nfound <- Nfound + 1
 		} else {
 			cat( "\nvelvetg:   unexpected reply;\n", ans)
+			Nfail <- Nfail + 1
 		}
 	}
 
@@ -92,8 +137,10 @@
 		ans <- system( "cutadapt --version", intern=T) 
 		if ( length(ans) == 1) {
 			cat( "\ncutdapt:   version =", ans[1], "      path =", exe)
+			Nfound <- Nfound + 1
 		} else {
 			cat( "\ncutadapt:   unexpected reply;\n", ans)
+			Nfail <- Nfail + 1
 		}
 	}
 
@@ -104,6 +151,7 @@
 		cat( "\n\nNote:  Bowtie1 only used for building genome detectability files when adding new genomes...\n")
 	} else {
 		cat( "\nBowtie1:   version =", ans, "     path =", as.character(Sys.which("bowtie")))
+		Nfound <- Nfound + 1
 	}
 
 	if ( Nfail == 0) {
@@ -112,6 +160,8 @@
 	}
 	if ( Nfail > 0) {
 		cat( "\n\nFailled to find and verify ", Nfail, " programs wanted by DuffyNGS.\n")
-		cat( "\nCheck your PATH environment variable etc...")
+		cat( "\nCheck your PATH environment variable etc...\n")
 	}
+	
+	return( list( "N_Found"=Nfound, "N_Fail"=Nfail))
 }
