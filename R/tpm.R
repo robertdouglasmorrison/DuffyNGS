cb5f6b906ffb4b1580b6c975e4e5fb03c0c0bbf7
Robert Morrison <robert.morrison@nih.gov>
2023-06-15 10:49:37 -0700

    changing TPM code to better correct for very low read counts

--- R/tpm.R
+++ R/tpm.R
@@ -7,23 +7,30 @@ tpm <- function( readCount, geneLen, readLen=100, minReadsPerSpecies=10000) {
 
 	# the sum of all genes is the T term in the denominator
 
-	# there is a chance of having very small number of aligned reads, which breaks TPM
+	# there is a chance of having very small number of aligned reads, which breaks TPM math
 	readCountForT <- readCount
-	totalReads <- sum( readCount)
+	totalReadsForT <- totalReads <- sum( readCount)
 	if (totalReads < minReadsPerSpecies) {
-		if ( all( readCountForT == 0)) readCountForT <- rep.int(1,length(readCount))
-		readCountForT <- readCountForT * minReadsPerSpecies / sum( readCountForT)
+		# scale everyone to look like we got the minimum number of reads
+		# special catch if zero reads to all genes
+		if ( all( readCountForT == 0)) {
+			readCountForT <- rep.int(1,length(readCount))
+			totalReadsForT <- sum( readCountForT)
+			readCountForT <- readCountForT * minReadsPerSpecies / totalReadsForT
+		} else {
+			readCountForT <- readCountForT * minReadsPerSpecies / totalReadsForT
+			readCount <- readCount * minReadsPerSpecies / totalReads
+		}
 	}
+
 	# first normalize the read counts by gene length
 	perK <- readLen / geneLen
 	RperG <- readCountForT * perK
 	T <- sum(RperG)
 
-
 	# make the TPM units for each gene
 	tpm <- (readCount * readLen * 1000000) / (geneLen * T)
 
-	# trim to something sensible?...
 	return( tpm)
 }
 
