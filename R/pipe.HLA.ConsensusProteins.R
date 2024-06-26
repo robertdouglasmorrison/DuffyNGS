b38df9279c53d9fe2ea0652072c3e5babfd47375
Robert Morrison <robert.morrison@nih.gov>
2024-06-26 12:34:29 -0700

    preserving full allele names in MAFFT result

--- R/pipe.HLA.ConsensusProteins.R
+++ R/pipe.HLA.ConsensusProteins.R
@@ -690,6 +690,10 @@ ALL_HLA_GeneNames <- c( "HLA-A", "HLA-B", "HLA-C", "HLA-E", "HLA-DRA", "HLA-DRB1
 	tooLong <- which( nchar(showNames) > 5)
 	showNames[tooLong] <- sub( "^HLA\\-", "", showNames[tooLong])
 	text( (starts+stops)/2, smlY*0.65, showNames, font=2, cex=0.85)
+	dev.flush(); Sys.sleep(0.1)
+	plotFile <- paste( "HLA.AminoAcid.Proportions_By.", groupColumn, ".pdf", sep="")
+	printPlot( plotFile)
+	
 	
 	# done
 	outFile <- paste( "HLA.AminoAcid.Proportions_By.", groupColumn, ".csv", sep="")
